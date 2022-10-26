3753a8f96b1a121e5baf00a20bc8f2317df54c72
Richelle Thorpe <richelle.ak@gmail.com>
2022-10-26 11:02:49 -0700

    add for loop function
    Co-authored-by: Aitana Shough <aeshough@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+let countTo = 30;
+const countBy = 5;
+for (let i = 0; i <=countTo; i+= countBy) {
+  console.log(i);
+}
+
+let countTo = 50;
+const countBy = 7;
+for (let i = 0; i <=countTo; i+= countBy) {
+  console.log(i);
+}
+
+function checkForBlanks(){
+  if ((countBy <= 0) || (countTo <= 0)) {
+    return null;
+  })
+  else if ((countBy ===NaN) || (countTo === NaN)){
+    return null;
+  }
+};
+
+
+
+for (let index = 0; index <= 30; index += 5) {
+  console.log(index);
+}
